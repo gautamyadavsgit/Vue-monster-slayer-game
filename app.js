3e2a06b2f342d75533b9e327869f1630b0a1907f
gautamyadavsgit <gautaminit@gmail.com>
2022-11-02 09:57:53 +0530

    aded

--- app.js
+++ app.js
@@ -7,11 +7,33 @@ const App = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            attackRound: 0,
+            Winner: null,
         };
+    }, watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.Winner = 'Draw';
+            } else if (value <= 0) {
+                this.Winner = 'Monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                // A draw
+                this.Winner = 'Draw';
+            } else if (value <= 0) {
+                // Player Won
+                this.Winner = 'Player';
+            }
+        }
+
     },
+
     methods: {
         attackMonster() {
+            this.attackRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth = this.monsterHealth - attackValue;
             this.attackPlayer();
@@ -19,15 +41,43 @@ const App = Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomValue(8, 12);
             this.playerHealth = this.playerHealth - attackValue;
+        }, specialMonsterAttack() {
+            this.attackRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
+        }, HealPlayer() {
+            const HealValue = getRandomValue(12, 20);
+            if (this.playerHealth + HealValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth = this.playerHealth + HealValue;
+            }
+
+            this.attackPlayer();
+        },surrender(){
+            this.Winner = 'Monster';
+        },restart(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.attackRound = 0;
+            this.Winner = null;
         }
     },
     computed: {
-       monsterBarStyles(){
-        return {width:this.monsterHealth + '%'};
-       },
-       playerBarStyles(){
-        return {width:this.playerHealth + '%'}
-    }
+        monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            }
+            return { width: this.monsterHealth + '%' };
+        },
+        playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            }
+
+            return { width: this.playerHealth + '%' }
+        }
     }
 
 });
